33bde4164042caeb4e7ea0588ceac64970372d47
stiven116 <stivensanchez003@gmail.com>
2015-02-03 16:33:12 +0000

    Se organizo lo de el tipo de documento en las operarias y en los instructores, y algo mas de modificaciones

--- db/migrate/20141128162753_create_operations.rb
+++ db/migrate/20141128162753_create_operations.rb
@@ -2,7 +2,7 @@ class CreateOperations < ActiveRecord::Migration
   def change
     create_table :operations do |t|
       t.string :nombre
-      t.string :sam
+      t.double :sam
       t.text :descripcion
       t.references :maquina, index: true
       t.references :modulo, index: true
